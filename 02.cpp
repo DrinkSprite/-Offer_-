006ebb549782b95812aaee7babb19600a6314ef3
DrinkSprite <38090721+DrinkSprite@users.noreply.github.com>
2019-11-14 22:58:29 +0800

    Create 02.cpp
    替换空格

---
+++ 02.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+	void replaceSpace(char *str,int length) {
+        char *str_cpy = (char*)malloc(sizeof(char)*length);
+        memcpy(str_cpy,str,sizeof(char)*length);
+        for(int i = 0,j = 0;i < length;i++,j++)
+        {
+            if(str_cpy[i] == ' ')
+            {
+                str[j] = '%';
+                str[j+1] = '2';
+                str[j+2] = '0';
+                j = j + 2;
+            }
+            else
+            {
+                str[j] = str_cpy[i];
+            }
+        }
+	}
+};
